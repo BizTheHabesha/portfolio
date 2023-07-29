ce04bd630f5c1241d78903250ad3478ccc19b10e
Biz <github@bisrat.dev>
2023-07-28 21:29:08 -0600

    center cards

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,7 +5,10 @@ import { Container, Stack } from "react-bootstrap";
 
 function Projects() {
 	return (
-		<Stack className="pt-4 d-flex flex-wrap" direction="horizontal" gap={3}>
+		<Stack
+			className="pt-4 d-flex flex-wrap justify-content-center"
+			direction="horizontal"
+			gap={3}>
 			{RawProjects.map((el) => el({ key: uuid() }))}
 		</Stack>
 	);
